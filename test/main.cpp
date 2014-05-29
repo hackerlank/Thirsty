c3cdbca6bb0fe87c9bdd808868e918812008dc1f
ichenq <ichenq@gmail.com>
2014-05-29 17:08:14 +0800

    add test case

---
+++ test/main.cpp
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <objbase.h>
+#include <gtest/gtest.h>
+
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "winmm.lib")
+
+
+class MyTestEnvironment : public testing::Environment
+{
+public:
+    void  SetUp()
+    {
+        ::CoInitialize(NULL);
+        srand((unsigned)time(NULL));
+    }
+
+    void  TearDown()
+    {
+        ::CoUninitialize();
+    }
+};
+
+
+int main(int argc, char* argv[])
+{
+    testing::AddGlobalTestEnvironment(new MyTestEnvironment);
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
