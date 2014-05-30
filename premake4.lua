bcc80d6c1b7076972c74daf9e60e7e5cd70d0ad4
ichenq <ichenq@gmail.com>
2014-05-30 13:28:20 +0800

    seperate projects, gtest and double-conversion

--- premake4.lua
+++ premake4.lua
@@ -5,24 +5,23 @@
 local BOOST_ROOT = os.getenv('BOOST_ROOT')
 
 solution 'Thirsty'
-    configurations {'Debug', 'Release'}    
+    configurations {'Debug', 'Release'}
     language 'C++'
     flags {'ExtraWarnings'}
     targetdir 'bin'
-    
+
     configuration 'Debug'
         defines { 'DEBUG' }
         flags { 'Symbols' }
-        
+
     configuration 'Release'
         defines { 'NDEBUG' }
         flags { 'Symbols', 'Optimize' }
-        
+
     project 'UnitTest'
         location 'build'
         kind 'ConsoleApp'
-        uuid "AB7D1C15-7A44-41a7-8864-230D8E345608"
-        
+        uuid 'AB7D1C15-7A44-41a7-8864-230D8E345608'
         defines
         {
             'WIN32',
@@ -33,28 +32,29 @@ solution 'Thirsty'
             '_SCL_SECURE_NO_WARNINGS',
             '_CRT_SECURE_NO_WARNINGS',
         }
-        files 
+        files
         {
-            'dep/gtest/src/gtest-all.cc',
             'test/*.h',
             'test/*.cpp',
         }
         includedirs
         {
             'src',
-            'dep/gtest',
             'dep/gtest/include',
             'dep/double-conversion/src',
             BOOST_ROOT,
         }
+        links
+        {
+            'libThirsty',
+            'libgtest',
+            'double-conversion',
+        }
 
-        links 'libThirsty'
-        
     project 'libThirsty'
         location 'build'
         kind 'StaticLib'
-        uuid "8701594A-72B8-4a6a-AEF3-6B41BBC33E65"
-        
+        uuid '8701594A-72B8-4a6a-AEF3-6B41BBC33E65'
         defines
         {
             'WIN32',
@@ -65,8 +65,7 @@ solution 'Thirsty'
             '_SCL_SECURE_NO_WARNINGS',
             '_CRT_SECURE_NO_WARNINGS',
         }
-        
-        files 
+        files
         {
             'src/**.h',
             'src/**.cpp',
@@ -76,4 +75,38 @@ solution 'Thirsty'
             'src',
             'dep/double-conversion/src',
             BOOST_ROOT,
+        }
+
+    project 'libgtest'
+        location 'build'
+        kind 'StaticLib'
+        uuid '31BC2F58-F374-4984-B490-F1F08ED02DD3'
+        defines
+        {
+        }
+        files
+        {
+            'dep/gtest/src/gtest-all.cc',
+        }
+        includedirs
+        {
+            'dep/gtest',
+            'dep/gtest/include',
+        }
+
+    project 'double-conversion'
+        location 'build'
+        kind 'StaticLib'
+        uuid 'AE6D2B6D-1CFB-48DE-A982-BAECACE31AE2'
+        defines
+        {
+        }
+        files
+        {
+            'dep/double-conversion/src/*.cc',
+            'dep/double-conversion/src/*.h',
+        }
+        includedirs
+        {
+            'dep/double-conversion/src',
         }
