d95ecb4153032c93ec29ebabfc38419c9b49c0d5
ichenq <ichenq@gmail.com>
2014-06-15 17:08:27 +0800

    test tcp script

---
+++ test/test-client.py
@@ -0,0 +1,22 @@
+import socket
+import binascii
+import struct
+import pdb
+
+
+def pack_data(data):
+    size = len(data)    
+    size_crc = binascii.crc32(struct.pack('i', size))
+    data_crc = binascii.crc32(data)
+    return struct.pack('iii',  size, size_crc, data_crc) + data
+
+def run_test():
+    s = create_socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('127.0.0.1', 32450))
+    data = pack_data('hello,kitty')
+    s.send(data)
+    print(s.recv(1024))
+
+    
+if __name__ == '__main__':
+    run_test()
