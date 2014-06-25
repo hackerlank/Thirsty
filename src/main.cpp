96367987549e8875cd7201ce936e7e3061dc4987
ichenq <ichenq@gmail.com>
2014-06-25 23:01:32 +0800

    main entry

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
-#include "net/TCPServer.h"
+
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    if (argc < 3)
+    if (argc < 2)
     {
-        cout << "Usage: Thirsty [host] [port].\n";
+        cout << "Usage: Thirsty [file].\n";
         return 1;
     }
-
-    boost::asio::io_service io_service;
-    TCPServer server(io_service);
-    server.Start(argv[1], argv[2]);
-    cout << "server start at " << argv[1] << ": " << argv[2] << endl;
-    io_service.run();
 
     return 0;
 }
