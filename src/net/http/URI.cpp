83f763b005f2905af7a8a6a3de087e221aa72382
ichenq <ichenq@gmail.com>
2014-06-26 17:50:08 +0800

    URI class

--- src/net/http/URI.cpp
+++ src/net/http/URI.cpp
@@ -13,19 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#if 0
-#include "folly/Uri.h"
+
+#include "URI.h"
 
 #include <ctype.h>
-#include <boost/regex.hpp>
+#include <regex>
+#include "core/Conv.h"
 
-namespace folly {
+using std::string;
 
 namespace {
 
-fbstring submatch(const boost::cmatch& m, size_t idx) {
+string submatch(const std::cmatch& m, size_t idx) {
   auto& sub = m[idx];
-  return fbstring(sub.first, sub.second);
+  return string(sub.first, sub.second);
 }
 
 template <class String>
@@ -38,15 +39,15 @@ void toLower(String& s) {
 }  // namespace
 
 Uri::Uri(StringPiece str) : port_(0) {
-  static const boost::regex uriRegex(
+  static const std::regex uriRegex(
       "([a-zA-Z][a-zA-Z0-9+.-]*):"  // scheme:
       "([^?#]*)"                    // authority and path
       "(?:\\?([^#]*))?"             // ?query
       "(?:#(.*))?");                // #fragment
-  static const boost::regex authorityAndPathRegex("//([^/]*)(/.*)?");
+  static const std::regex authorityAndPathRegex("//([^/]*)(/.*)?");
 
-  boost::cmatch match;
-  if (UNLIKELY(!boost::regex_match(str.begin(), str.end(), match, uriRegex))) {
+  std::cmatch match;
+  if (!std::regex_match(str.begin(), str.end(), match, uriRegex)) {
     throw std::invalid_argument(to<std::string>("invalid URI ", str));
   }
 
@@ -54,23 +55,23 @@ Uri::Uri(StringPiece str) : port_(0) {
   toLower(scheme_);
 
   StringPiece authorityAndPath(match[2].first, match[2].second);
-  boost::cmatch authorityAndPathMatch;
-  if (!boost::regex_match(authorityAndPath.begin(),
+  std::cmatch authorityAndPathMatch;
+  if (!std::regex_match(authorityAndPath.begin(),
                           authorityAndPath.end(),
                           authorityAndPathMatch,
                           authorityAndPathRegex)) {
     // Does not start with //, doesn't have authority
-    path_ = authorityAndPath.fbstr();
+    path_ = authorityAndPath.str();
   } else {
-    static const boost::regex authorityRegex(
+    static const std::regex authorityRegex(
         "(?:([^@:]*)(?::([^@]*))?@)?"  // username, password
         "(\\[[^\\]]*\\]|[^\\[:]*)"     // host (IP-literal (e.g. '['+IPv6+']',
                                        // dotted-IPv4, or named host)
         "(?::(\\d*))?");               // port
 
     auto authority = authorityAndPathMatch[1];
-    boost::cmatch authorityMatch;
-    if (!boost::regex_match(authority.first,
+    std::cmatch authorityMatch;
+    if (!std::regex_match(authority.first,
                             authority.second,
                             authorityMatch,
                             authorityRegex)) {
@@ -94,8 +95,8 @@ Uri::Uri(StringPiece str) : port_(0) {
   fragment_ = submatch(match, 4);
 }
 
-fbstring Uri::authority() const {
-  fbstring result;
+string Uri::authority() const {
+  string result;
 
   // Port is 5 characters max and we have up to 3 delimiters.
   result.reserve(host().size() + username().size() + password().size() + 8);
@@ -115,13 +116,13 @@ fbstring Uri::authority() const {
 
   if (port() != 0) {
     result.push_back(':');
-    toAppend(port(), &result);
+    toAppend(&result, port());
   }
 
   return result;
 }
 
-fbstring Uri::hostname() const {
+string Uri::hostname() const {
   if (host_.size() > 0 && host_[0] == '[') {
     // If it starts with '[', then it should end with ']', this is ensured by
     // regex
@@ -130,5 +131,26 @@ fbstring Uri::hostname() const {
   return host_;
 }
 
-}  // namespace folly
-#endif
+string Uri::str() const {
+    string str;
+    toAppend(&str, scheme_, "://");
+    if (!password_.empty()) {
+        toAppend(&str, username_, ":", password_, "@");
+    }
+    else if (!username_.empty()) {
+        toAppend(&str, username_, "@");
+    }
+    toAppend(&str, host_);
+    if (port_ != 0) {
+        toAppend(&str, ":", port_);
+    }
+    toAppend(&str, path_);
+    if (!query_.empty()) {
+        toAppend(&str, "?", query_);
+    }
+    if (!fragment_.empty()) {
+        toAppend(&str, "#", fragment_);
+    }
+    return str;
+}
+
