01e6dbb0554f0cb513c12d8c184f3989579c3eed
ichenq <ichenq@gmail.com>
2014-06-27 11:25:28 +0800

    unittest of HttpClient

---
+++ test/HttpClientTest.cpp
@@ -0,0 +1,24 @@
+#include "net/http/HttpClient.h"
+#include <gtest/gtest.h>
+
+TEST(HttpClient, SendRequest)
+{
+    boost::asio::io_service io_service;
+
+    HttpClient http_client(io_service, "www.qq.com", "http");
+    std::string request =
+        "GET /index.html HTTP/1.1\r\n"
+        "Host: 0.0.0.0=8000\r\n"
+        "User-Agent: Chrome\r\n"
+        "Keep-Alive: 100\r\n"
+        "Connection: keep-alive\r\n"
+        "\r\n";
+
+    http_client.SendRequest(request, [&](StringPiece s)
+    {
+        cout << string(s.data(), s.size()) << endl;
+    });
+
+    auto r = io_service.run();
+    EXPECT_EQ(r, 1);
+}
