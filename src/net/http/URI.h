9969054b26d703f6e52a8e485e499041cb8d513f
ichenq <ichenq@gmail.com>
2014-07-01 16:13:14 +0800

    Uri using #pragma once instruction

--- src/net/http/URI.h
+++ src/net/http/URI.h
@@ -14,8 +14,7 @@
  * limitations under the License.
  */
 
-#ifndef FOLLY_URI_H_
-#define FOLLY_URI_H_
+#pragma once
 
 #include <string>
 #include "core/Range.h"
@@ -36,52 +35,50 @@
  * UriEscapeMode::QUERY) (for the query, but probably only after splitting at
  * '&' to identify the individual parameters).
  */
-class Uri {
- public:
-  /**
-   * Parse a Uri from a string.  Throws std::invalid_argument on parse error.
-   */
-  explicit Uri(StringPiece str);
+class Uri 
+{
+public:
+    /**
+     * Parse a Uri from a string.  Throws std::invalid_argument on parse error.
+     */
+    explicit Uri(StringPiece str);
 
-  const std::string& scheme() const { return scheme_; }
-  const std::string& username() const { return username_; }
-  const std::string& password() const { return password_; }
-  /**
-   * Get host part of URI. If host is an IPv6 address, square brackets will be
-   * returned, for example: "[::1]".
-   */
-  const std::string& host() const { return host_; }
-  /**
-   * Get host part of URI. If host is an IPv6 address, square brackets will not
-   * be returned, for exmaple "::1"; otherwise it returns the same thing as
-   * host().
-   *
-   * hostname() is what one needs to call if passing the host to any other tool
-   * or API that connects to that host/port; e.g. getaddrinfo() only understands
-   * IPv6 host without square brackets
-   */
-  std::string hostname() const;
-  uint16_t port() const { return port_; }
-  const std::string& path() const { return path_; }
-  const std::string& query() const { return query_; }
-  const std::string& fragment() const { return fragment_; }
+    const std::string& scheme() const { return scheme_; }
+    const std::string& username() const { return username_; }
+    const std::string& password() const { return password_; }
+    /**
+     * Get host part of URI. If host is an IPv6 address, square brackets will be
+     * returned, for example: "[::1]".
+     */
+    const std::string& host() const { return host_; }
+    /**
+     * Get host part of URI. If host is an IPv6 address, square brackets will not
+     * be returned, for exmaple "::1"; otherwise it returns the same thing as
+     * host().
+     *
+     * hostname() is what one needs to call if passing the host to any other tool
+     * or API that connects to that host/port; e.g. getaddrinfo() only understands
+     * IPv6 host without square brackets
+     */
+    std::string hostname() const;
+    uint16_t port() const { return port_; }
+    const std::string& path() const { return path_; }
+    const std::string& query() const { return query_; }
+    const std::string& fragment() const { return fragment_; }
 
-  std::string authority() const;
+    std::string authority() const;
 
-  std::string str() const;
+    std::string str() const;
 
-  void setPort(uint16_t port) {port_ = port;}
+    void setPort(uint16_t port) { port_ = port; }
 
- private:
-  std::string   scheme_;
-  std::string   username_;
-  std::string   password_;
-  std::string   host_;
-  uint16_t      port_;
-  std::string   path_;
-  std::string   query_;
-  std::string   fragment_;
+private:
+    std::string   scheme_;
+    std::string   username_;
+    std::string   password_;
+    std::string   host_;
+    uint16_t      port_;
+    std::string   path_;
+    std::string   query_;
+    std::string   fragment_;
 };
-
-
-#endif /* FOLLY_URI_H_ */
