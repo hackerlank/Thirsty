fd17f9a6905e3a5fb22ed6731a9d0d26dee5c86d
ichenq <ichenq@gmail.com>
2014-07-01 15:12:03 +0800

    CpuId: move output function to unittest file

--- src/core/CpuId.h
+++ src/core/CpuId.h
@@ -36,7 +36,7 @@ public:
         // as they do on Intel
         c_ = 0;
         d_ = 0;
-#if defined(_MSC_VER) && _MSC_VER >= 1600
+#if defined(_MSC_VER)
         int cpu_info[4] = {};
         __cpuid(cpu_info, 1);
         c_ = cpu_info[2];
@@ -120,72 +120,3 @@ private:
     uint32_t c_;  // ECX
     uint32_t d_;  // EDX
 };
-
-template <typename S>
-inline S& operator << (S& out, const CpuId& cpu)
-{
-#define FN(a, x)    a.x()
-#define OUTPUT(x)   out << #x << ":" << FN(cpu, x) << "\n"
-    OUTPUT(sse3);
-    OUTPUT(pclmuldq);
-    OUTPUT(dtes64);
-    OUTPUT(monitor);
-    OUTPUT(dscpl);
-    OUTPUT(vmx);
-    OUTPUT(smx);
-    OUTPUT(eist);
-    OUTPUT(tm2);
-    OUTPUT(ssse3);
-    OUTPUT(fma);
-    OUTPUT(cx16);
-    OUTPUT(xtpr);
-    OUTPUT(pdcm);
-    OUTPUT(pcid);
-    OUTPUT(sse41);
-    OUTPUT(sse42);
-    OUTPUT(x2apic);
-    OUTPUT(movbe);
-    OUTPUT(popcnt);
-    OUTPUT(tscdeadline);
-    OUTPUT(aes);
-    OUTPUT(xsave);
-    OUTPUT(osxsave);
-    OUTPUT(avx);
-    OUTPUT(osxsave);
-    OUTPUT(f16c);
-    OUTPUT(rdrand);
-    OUTPUT(fpu);
-    OUTPUT(vme);
-    OUTPUT(de);
-    OUTPUT(pse);
-    OUTPUT(tsc);
-    OUTPUT(msr);
-    OUTPUT(pae);
-    OUTPUT(mce);
-    OUTPUT(cx8);
-    OUTPUT(apic);
-    OUTPUT(sep);
-    OUTPUT(apic);
-    OUTPUT(mtrr);
-    OUTPUT(apic);
-    OUTPUT(pge);
-    OUTPUT(mca);
-    OUTPUT(cmov);
-    OUTPUT(pat);
-    OUTPUT(pse36);
-    OUTPUT(psn);
-    OUTPUT(clfsh);
-    OUTPUT(ds);
-    OUTPUT(acpi);
-    OUTPUT(mmx);
-    OUTPUT(fxsr);
-    OUTPUT(sse);
-    OUTPUT(sse2);
-    OUTPUT(ss);
-    OUTPUT(htt);
-    OUTPUT(tm);
-    OUTPUT(pbe);
-#undef OUTPUT
-#undef FN
-    return out;
-}
--- test/CpuIdTest.cpp
+++ test/CpuIdTest.cpp
@@ -18,6 +18,8 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
+using namespace std;
+
 TEST(CpuId, Simple)
 {
     // All CPUs should support MMX
@@ -25,8 +27,77 @@ TEST(CpuId, Simple)
     EXPECT_TRUE(id.mmx());
 }
 
+
+static void OutputCpuid(CpuId cpuid)
+{
+    cout << "Cpuid: " << endl;
+#define FN(a, x)    a.x()
+#define OUTPUT(x)   cout << "\t" << #x << ": " << FN(cpuid, x) << endl
+    OUTPUT(sse3);
+    OUTPUT(pclmuldq);
+    OUTPUT(dtes64);
+    OUTPUT(monitor);
+    OUTPUT(dscpl);
+    OUTPUT(vmx);
+    OUTPUT(smx);
+    OUTPUT(eist);
+    OUTPUT(tm2);
+    OUTPUT(ssse3);
+    OUTPUT(fma);
+    OUTPUT(cx16);
+    OUTPUT(xtpr);
+    OUTPUT(pdcm);
+    OUTPUT(pcid);
+    OUTPUT(sse41);
+    OUTPUT(sse42);
+    OUTPUT(x2apic);
+    OUTPUT(movbe);
+    OUTPUT(popcnt);
+    OUTPUT(tscdeadline);
+    OUTPUT(aes);
+    OUTPUT(xsave);
+    OUTPUT(osxsave);
+    OUTPUT(avx);
+    OUTPUT(osxsave);
+    OUTPUT(f16c);
+    OUTPUT(rdrand);
+    OUTPUT(fpu);
+    OUTPUT(vme);
+    OUTPUT(de);
+    OUTPUT(pse);
+    OUTPUT(tsc);
+    OUTPUT(msr);
+    OUTPUT(pae);
+    OUTPUT(mce);
+    OUTPUT(cx8);
+    OUTPUT(apic);
+    OUTPUT(sep);
+    OUTPUT(apic);
+    OUTPUT(mtrr);
+    OUTPUT(apic);
+    OUTPUT(pge);
+    OUTPUT(mca);
+    OUTPUT(cmov);
+    OUTPUT(pat);
+    OUTPUT(pse36);
+    OUTPUT(psn);
+    OUTPUT(clfsh);
+    OUTPUT(ds);
+    OUTPUT(acpi);
+    OUTPUT(mmx);
+    OUTPUT(fxsr);
+    OUTPUT(sse);
+    OUTPUT(sse2);
+    OUTPUT(ss);
+    OUTPUT(htt);
+    OUTPUT(tm);
+    OUTPUT(pbe);
+#undef OUTPUT
+#undef FN    
+}
+
 TEST(CpuId, Output)
 {
-    CpuId id;
-    std::cout << id << std::endl;
+    CpuId cpuid;
+    OutputCpuid(cpuid);
 }
