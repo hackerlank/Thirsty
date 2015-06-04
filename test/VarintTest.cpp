0d3b0390bd5737acb9f3fd5dab7158827e5b86a9
ichenq <ichenq@gmail.com>
2015-06-04 12:00:56 +0800

    Update Varint

--- test/VarintTest.cpp
+++ test/VarintTest.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Facebook, Inc.
+ * Copyright 2015 Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,58 +14,77 @@
  * limitations under the License.
  */
 
-#include "core/Varint.h"
-#include <gtest/gtest.h>
-#include <ctime>
+#include "Varint.h"
 #include <array>
 #include <initializer_list>
 #include <random>
 #include <vector>
-#include "logging.h"
+#include <gtest/gtest.h>
+#include "Logging.h"
 #include "Benchmark.h"
-#include "core/Random.h"
 
-
-
-void testVarint(uint64_t val, std::initializer_list<uint8_t> bytes) 
+inline int32_t GenRandomSeed()
 {
-    size_t n = bytes.size();
-    ByteRange expected(&*bytes.begin(), n);
-
-    {
-        uint8_t buf[kMaxVarintLength64];
-        EXPECT_EQ(expected.size(), encodeVarint(val, buf));
-        EXPECT_TRUE(ByteRange(buf, expected.size()) == expected);
-    }
-
-    {
-        ByteRange r = expected;
-        uint64_t decoded = decodeVarint(r);
-        EXPECT_TRUE(r.empty());
-        EXPECT_EQ(val, decoded);
-    }
-
-    if (n < kMaxVarintLength64)
-    {
-        // Try from a full buffer too, different code path
-        uint8_t buf[kMaxVarintLength64];
-        memcpy(buf, &*bytes.begin(), n);
-
-        uint8_t fills[] = { 0, 0x7f, 0x80, 0xff };
-
-        for (uint8_t fill : fills)
-        {
-            memset(buf + n, fill, kMaxVarintLength64 - n);
-            ByteRange r(buf, kMaxVarintLength64);
-            uint64_t decoded = decodeVarint(r);
-            EXPECT_EQ(val, decoded);
-            EXPECT_EQ(kMaxVarintLength64 - n, r.size());
-        }
-    }
+    auto device = std::random_device();
+    return device();
 }
 
-TEST(Varint, Simple) 
-{
+DEFINE_int32(random_seed, GenRandomSeed(), "random seed");
+
+
+void testVarint(uint64_t val, std::initializer_list<uint8_t> bytes) {
+  size_t n = bytes.size();
+  ByteRange expected(&*bytes.begin(), n);
+
+  {
+    uint8_t buf[kMaxVarintLength64];
+    EXPECT_EQ(expected.size(), encodeVarint(val, buf));
+    EXPECT_TRUE(ByteRange(buf, expected.size()) == expected);
+  }
+
+  {
+    ByteRange r = expected;
+    uint64_t decoded = decodeVarint(r);
+    EXPECT_TRUE(r.empty());
+    EXPECT_EQ(val, decoded);
+  }
+
+  if (n < kMaxVarintLength64) {
+    // Try from a full buffer too, different code path
+    uint8_t buf[kMaxVarintLength64];
+    memcpy(buf, &*bytes.begin(), n);
+
+    uint8_t fills[] = {0, 0x7f, 0x80, 0xff};
+
+    for (uint8_t fill : fills) {
+      memset(buf + n, fill, kMaxVarintLength64 - n);
+      ByteRange r(buf, kMaxVarintLength64);
+      uint64_t decoded = decodeVarint(r);
+      EXPECT_EQ(val, decoded);
+      EXPECT_EQ(kMaxVarintLength64 - n, r.size());
+    }
+  }
+}
+
+TEST(Varint, Interface) {
+  // Make sure decodeVarint() accepts all of StringPiece, MutableStringPiece,
+  // ByteRange, and MutableByteRange.
+  char c = 0;
+
+  StringPiece sp(&c, 1);
+  EXPECT_EQ(decodeVarint(sp), 0);
+
+  MutableStringPiece msp(&c, 1);
+  EXPECT_EQ(decodeVarint(msp), 0);
+
+  ByteRange br(reinterpret_cast<unsigned char*>(&c), 1);
+  EXPECT_EQ(decodeVarint(br), 0);
+
+  MutableByteRange mbr(reinterpret_cast<unsigned char*>(&c), 1);
+  EXPECT_EQ(decodeVarint(mbr), 0);
+}
+
+TEST(Varint, Simple) {
   testVarint(0, {0});
   testVarint(1, {1});
   testVarint(127, {127});
@@ -80,8 +99,7 @@ TEST(Varint, Simple)
              {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01});
 }
 
-TEST(ZigZag, Simple) 
-{
+TEST(ZigZag, Simple) {
   EXPECT_EQ(0, encodeZigZag(0));
   EXPECT_EQ(1, encodeZigZag(-1));
   EXPECT_EQ(2, encodeZigZag(1));
@@ -97,33 +115,30 @@ TEST(ZigZag, Simple)
 
 namespace {
 
-const size_t kNumValues = 1000;
+constexpr size_t kNumValues = 1000;
 std::vector<uint64_t> gValues;
 std::vector<uint64_t> gDecodedValues;
 std::vector<uint8_t> gEncoded;
 
-void generateRandomValues() 
-{
-    auto seed = time(0);
-    std::mt19937 rng(seed);
+void generateRandomValues() {
+  LOG(INFO) << "Random seed is " << FLAGS_random_seed;
+  std::mt19937 rng(FLAGS_random_seed);
 
-    // Approximation of power law
-    std::uniform_int_distribution<int> numBytes(1, 8);
-    std::uniform_int_distribution<int> byte(0, 255);
+  // Approximation of power law
+  std::uniform_int_distribution<int> numBytes(1, 8);
+  std::uniform_int_distribution<int> byte(0, 255);
 
-    gValues.resize(kNumValues);
-    gDecodedValues.resize(kNumValues);
-    gEncoded.resize(kNumValues * kMaxVarintLength64);
-    for (size_t i = 0; i < kNumValues; ++i) 
-    {
-        int n = numBytes(rng);
-        uint64_t val = 0;
-        for (size_t j = 0; j < n; ++j) 
-        {
-            val = (val << 8) + byte(rng);
-        }
-        gValues[i] = val;
+  gValues.resize(kNumValues);
+  gDecodedValues.resize(kNumValues);
+  gEncoded.resize(kNumValues * kMaxVarintLength64);
+  for (size_t i = 0; i < kNumValues; ++i) {
+    int n = numBytes(rng);
+    uint64_t val = 0;
+    for (int j = 0; j < n; ++j) {
+      val = (val << 8) + byte(rng);
     }
+    gValues[i] = val;
+  }
 }
 
 // Benchmark results (Intel(R) Xeon(R) CPU E5-2660 0 @ 2.20GHz, Linux x86_64)
@@ -146,34 +161,28 @@ void generateRandomValues()
 // VarintDecoding                                              12.60us   79.37K
 // ============================================================================
 
-BENCHMARK(VarintEncoding, iters) 
-{
-    uint8_t* start = &(*gEncoded.begin());
-    uint8_t* p = start;
-    bool empty = (iters == 0);
-    while (iters--)
-    {
-        p = start;
-        for (auto& v : gValues)
-        {
-            p += encodeVarint(v, p);
-        }
+BENCHMARK(VarintEncoding, iters) {
+  uint8_t* start = &(*gEncoded.begin());
+  uint8_t* p = start;
+  bool empty = (iters == 0);
+  while (iters--) {
+    p = start;
+    for (auto& v : gValues) {
+      p += encodeVarint(v, p);
     }
+  }
 
-    gEncoded.erase(gEncoded.begin() + (p - start), gEncoded.end());
+  gEncoded.erase(gEncoded.begin() + (p - start), gEncoded.end());
 }
 
-BENCHMARK(VarintDecoding, iters) 
-{
-    while (iters--) 
-    {
-        size_t i = 0;
-        ByteRange range(&(*gEncoded.begin()), &(*gEncoded.end()));
-        while (!range.empty()) 
-        {
-            gDecodedValues[i++] = decodeVarint(range);
-        }
+BENCHMARK(VarintDecoding, iters) {
+  while (iters--) {
+    size_t i = 0;
+    ByteRange range(&(*gEncoded.begin()), &(*gEncoded.end()));
+    while (!range.empty()) {
+      gDecodedValues[i++] = decodeVarint(range);
     }
+  }
 }
 
 }  // namespace
